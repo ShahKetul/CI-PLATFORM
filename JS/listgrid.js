c31d8427c22f9fa72b8828d6770a522c89f79f7b
ShahKetul <ketulsshah2002@gmail.com>
2023-03-02 18:01:15 +0530

    final commit

--- JS/listgrid.js
+++ JS/listgrid.js
@@ -8,14 +8,16 @@ function showList(e) {
     var $durationnonelist=$('.durationnonelist');
     var $butn_skill_seat_duration= $('.only_grid_butn_skill_seat_duration');
     var $only_list_butn_skill_seat_duration =$('.only_list_butn_skill_seat_duration');
+    var $final_hr = $('.final_hr');
     e.preventDefault();
     $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
     $bor.hasClass('list-view') ? $bor.addClass('border-0') : $bor.removeClass('border-top');
     $onlylistcol1.hasClass('list-view') ? $onlylistcol1.addClass('d-flex') : $onlylistcol1.removeClass('d-none');
-    $onlygrid.hasClass('list-view') ? $onlygrid.removeClass('.onlygrid') : $onlygrid.addClass('d-none');
+    $onlygrid.hasClass('list-view') ? $onlygrid.removeClass('.onlygrid'):$onlygrid.addClass('d-none');
     $durationnonelist.hasClass('list-view') ? $durationnonelist.removeClass('.durationnonelist') : $durationnonelist.addClass('d-none');
-    $butn_skill_seat_duration.hasClass('list-view') ? $butn_skill_seat_duration.removeClass('.onlygrid') : $butn_skill_seat_duration.addClass('d-none');
-    $only_list_butn_skill_seat_duration.hasClass('list-view') ? $only_list_butn_skill_seat_duration.addClass('.$only_list_butn_skill_seat_duration') : $only_list_butn_skill_seat_duration.removeClass('d-none');
+    $butn_skill_seat_duration.hasClass('list-view') ? $butn_skill_seat_duration.removeClass('d-none') : $butn_skill_seat_duration.addClass('d-none');
+    $only_list_butn_skill_seat_duration.hasClass('list-view') ? $only_list_butn_skill_seat_duration.addClass('.only_list_butn_skill_seat_duration') : $only_list_butn_skill_seat_duration.removeClass('d-none');
+    $final_hr.hasClass('list-view') ? $final_hr.removeClass('d-none') : $final_hr.addClass('d-none');
   }
   function gridList(e) {
     var $gridCont = $('.grid-container')
@@ -25,10 +27,11 @@ function showList(e) {
     var $durationnonelist=$('.durationnonelist');
     var $butn_skill_seat_duration= $('.only_grid_butn_skill_seat_duration');
     var $only_list_butn_skill_seat_duration =$('.only_list_butn_skill_seat_duration');
+    var $final_hr = $('.final_hr');
     // e.preventDefault();
     $gridCont.removeClass('list-view');
     $onlylistcol1.addClass('d-none');
-    $onlygrid.removeClass('d-none');
+    // $onlygrid.removeClass('d-none');
     $onlygrid.addClass('d-flex');
     $durationnonelist.removeClass('d-none');
     $durationnonelist.addClass('.durationnonelist');
@@ -36,11 +39,13 @@ function showList(e) {
     $butn_skill_seat_duration.removeClass('d-none');
     $butn_skill_seat_duration.addClass('.butn_skill_seat_duration');
     $only_list_butn_skill_seat_duration.addClass('d-none');
-   // $onlygrid.addClass('.onlygrid');
+   $onlygrid.addClass('.onlygrid');
+   $final_hr.removeClass('d-none')
   }
   
   $(document).on('click', '.btn-grid', gridList);
   $(document).on('click', '.btn-list', showList);
+  
 
   var checkboxes = document.querySelectorAll(".checkbox");
 
